186317a3a98baa1bc84c049367cc267ffbb43d13
Toms-Lipins-221RDB089 <125195653+Toms-Lipins-221RDB089@users.noreply.github.com>
2023-04-16 15:10:37 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -3,8 +3,14 @@
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
+    text = input()
+    if 'F' in text:
+        with open('/workspaces/string-pattern-Toms-Lipins-221RDB089/tests/06', 'r') as f:
+            line = f.readline()
+            line2=f.readline()
+    elif 'I' in text:
+       line = input()
+       line2=input()
     # after input type choice
     # read two lines 
     # first line is pattern 
@@ -13,7 +19,7 @@ def read_input():
     # return both lines in one return
     
     # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    return (line.rstrip(), line2.rstrip())
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
@@ -23,7 +29,48 @@ def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
 
     # and return an iterable variable
-    return [0]
+
+    M = len(pattern) 
+    N = len(text)
+    i = 0
+    j = 0
+    p = 0    
+    t = 0    
+    h = 1
+ 
+    
+    for i in xrange(M-1):
+        h = (h * d)% q
+ 
+   
+    for i in xrange(M):
+        p = (d * p + ord(pat[i]))% q
+        t = (d * t + ord(txt[i]))% q
+ 
+    
+    for i in xrange(N-M + 1):
+       
+        if p == t:
+           
+            for j in xrange(M):
+                if txt[i + j] != pat[j]:
+                    break
+ 
+            j+= 1
+          
+            if j == M:
+                 str(i)
+ 
+    
+        if i < N-M:
+            t = (d*(t-ord(txt[i])*h) + ord(txt[i + M]))% q
+ 
+            
+            if t < 0:
+                t = t + q
+
+
+    return str(i)
 
 
 # this part launches the functions
